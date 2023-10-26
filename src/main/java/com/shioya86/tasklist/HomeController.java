f3353996ccee2c95b5fe01e9f179b11a33300777
shara <shara@ml.harashio.jp>
2023-10-26 22:52:32 +0900

    add: HomeController hello view

--- src/main/java/com/shioya86/tasklist/HomeController.java
+++ src/main/java/com/shioya86/tasklist/HomeController.java
@@ -2,6 +2,7 @@ package com.shioya86.tasklist;
 
 import ch.qos.logback.core.util.Loader;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.time.LocalDateTime;
@@ -9,17 +10,8 @@ import java.time.LocalDateTime;
 @Controller
 public class HomeController {
     @RequestMapping("/hello")
-    @ResponseBody
-    String hello() {
-        return """
-                <html>
-                    <head><title>Hello</title></head>
-                    <body>
-                        <h1>Hello</h1>
-                        It works!<br>
-                        現在時刻は%sです。
-                    </body>
-                </html>
-               """.formatted(LocalDateTime.now());
+    String hello(Model model) {
+        model.addAttribute("time", LocalDateTime.now());
+        return "hello";
     }
 }
