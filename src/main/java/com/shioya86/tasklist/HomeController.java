b5ce7d8be68c5eb57053e137007a516141bd0b15
shara <shara@ml.harashio.jp>
2023-11-01 15:41:48 +0900

    タスクの削除処理の追加

--- src/main/java/com/shioya86/tasklist/HomeController.java
+++ src/main/java/com/shioya86/tasklist/HomeController.java
@@ -1,21 +1,25 @@
 package com.shioya86.tasklist;
 
-import ch.qos.logback.core.util.Loader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Controller
 public class HomeController {
     record TaskItem(String id, String task, String deadline, boolean done) {}
-    private List<TaskItem> taskItems = new ArrayList<>();
+    private final TaskListDao dao;
+
+    @Autowired
+    HomeController(TaskListDao dao) {
+        this.dao = dao;
+    }
+
     @RequestMapping("/hello")
     String hello(Model model) {
         model.addAttribute("time", LocalDateTime.now());
@@ -24,6 +28,7 @@ public class HomeController {
 
     @GetMapping("/list")
     String listItems(Model model) {
+        List<TaskItem> taskItems = dao.findAll();
         model.addAttribute("taskList", taskItems);
         return "home";
     }
@@ -33,8 +38,15 @@ public class HomeController {
                    @RequestParam("deadline") String deadline) {
         String id = UUID.randomUUID().toString().substring(0, 8);
         TaskItem item = new TaskItem(id, task, deadline, false);
-        taskItems.add(item);
+        dao.add(item);
 
+        return "redirect:/list";
+    }
+
+    @GetMapping("/delete")
+    String deleteItem(@RequestParam("id") String id) {
+        System.out.println("id = '" + id + "'");
+        dao.delete(id);
         return "redirect:/list";
     }
 }
---
+++ src/main/java/com/shioya86/tasklist/TaskListDao.java
@@ -0,0 +1,50 @@
+package com.shioya86.tasklist;
+
+import com.shioya86.tasklist.HomeController.TaskItem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class TaskListDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    TaskListDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void add(TaskItem taskItem) {
+        SqlParameterSource param = new BeanPropertySqlParameterSource(taskItem);
+        SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("tasklist");
+        insert.execute(param);
+    }
+
+    public int delete(String id) {
+        int number = jdbcTemplate.update("delete from tasklist where id = ?", id);
+        return number;
+    }
+
+    public List<TaskItem> findAll() {
+        String query = "select * from tasklist";
+
+        List<Map<String, Object>> result = jdbcTemplate.queryForList(query);
+        List<TaskItem> taskItems = result.stream()
+                .map((Map<String, Object> row) -> new TaskItem(
+                        row.get("id").toString(),
+                        row.get("task").toString(),
+                        row.get("deadline").toString(),
+                        (Boolean) row.get("done")))
+                .toList();
+
+        return taskItems;
+    }
+}
